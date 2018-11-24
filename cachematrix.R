1e72364d41ce3c1811728dc3e6ca2feb066806dd
easaltykova <easaltykova@edu.hse.ru>
2018-11-24 20:11:43 +0300

    week3 ass

--- cachematrix.R
+++ cachematrix.R
@@ -1,50 +1,31 @@
-<<<<<<< HEAD
-## These functions written for Coursera R Programming course
+## These functions written for of Coursera R Programming course
 
-##creates a special matrix object that can cache its inverse
-
+## This function creates a matrix object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
-  i <- NULL                             
-  set <- function(y) {                   
-    x <<- y                            
-    i <<- NULL                       
+  makeCacheMatrix <- function(x = matrix()) {
+    inv <- NULL                             
+    set <- function(y) {               
+      x <<- y                             
+      inv <<- NULL                       
+    }
+    get <- function() x                    
+    
+    setinverse <- function(inverse) inv <<- inverse  
+    getinverse <- function() inv                     
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
   }
-  get <- function() x                    
   
-  setinverse <- function(inverse) i <<- inverse  
-  getinverse <- function() i                     
-  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)  
-  
-}
 
-
-##computes the inverse of the matrix returned by makeCacheMatrix above.
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-  i <- x$getinverse()
-  if(!is.null(i)) {
-    message("get cache data")
-    return(i)
+  ## This function computes the inverse of the matri" returned by makeCacheMatrix
+  cacheSolve <- function(x, ...) {
+    ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinverse()
+    if(!is.null(inv)) {
+      message("getting data")
+      return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setinverse(inv)
+    inv
   }
-  data <- x$get()
-  i <- solve(data, ...)
-  x$setinverse(i)
-  i
-=======
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
->>>>>>> 7f657dd22ac20d22698c53b23f0057e1a12c09b7
-}
